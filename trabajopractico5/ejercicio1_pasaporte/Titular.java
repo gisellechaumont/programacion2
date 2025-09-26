a2bf1bce2520890eb027d872786d0d24d1b29726
Giselle <69175205+gisellechaumont@users.noreply.github.com>
2025-09-26 10:45:07 -0300

    Create Titular.java

---
+++ trabajopractico5/ejercicio1_pasaporte/Titular.java
@@ -0,0 +1,21 @@
+public class Titular {
+    private String nombre;
+    private String dni;
+
+    private Pasaporte pasaporte; // asociación bidireccional
+
+    public Titular(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    // Getters y setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getDni() { return dni; }
+    public void setDni(String dni) { this.dni = dni; }
+
+    public Pasaporte getPasaporte() { return pasaporte; }
+    public void setPasaporte(Pasaporte pasaporte) { this.pasaporte = pasaporte; }
+}
