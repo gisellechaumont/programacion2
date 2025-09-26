3f1dbe70349d0b36f9ec1446545c9e96503e2b47
Giselle <69175205+gisellechaumont@users.noreply.github.com>
2025-09-26 11:18:07 -0300

    Create Paciente.java

---
+++ trabajopractico5/ejercicio9_citaMedica/Paciente.java
@@ -0,0 +1,22 @@
+public class Paciente {
+    private String nombre;
+    private String obraSocial;
+
+    public Paciente(String nombre, String obraSocial) {
+        this.nombre = nombre;
+        this.obraSocial = obraSocial;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getObraSocial() {
+        return obraSocial;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente [nombre=" + nombre + ", obraSocial=" + obraSocial + "]";
+    }
+}
