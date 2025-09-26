9eb7dcfd0a5a984a093a3aeef53a230b7e77d304
Giselle <69175205+gisellechaumont@users.noreply.github.com>
2025-09-26 11:01:21 -0300

    Create Bateria.java

---
+++ trabajopractico5/ejercicio5_celular/Bateria.java
@@ -0,0 +1,17 @@
+public class Bateria {
+    private String modelo;
+    private int capacidad;
+
+    public Bateria(String modelo, int capacidad) {
+        this.modelo = modelo;
+        this.capacidad = capacidad;
+    }
+
+    public String getModelo() { return modelo; }
+    public int getCapacidad() { return capacidad; }
+
+    @Override
+    public String toString() {
+        return "Bateria[modelo=" + modelo + ", capacidad=" + capacidad + "]";
+    }
+}
