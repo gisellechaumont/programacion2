53ee749398e6849f59a1fa34f5b8a0af473a7cb8
Giselle <69175205+gisellechaumont@users.noreply.github.com>
2025-09-26 10:59:47 -0300

    Update Editorial.java

--- trabajopractico5/ejercicio3_libro/Editorial.java
+++ trabajopractico5/ejercicio3_libro/Editorial.java
@@ -7,10 +7,11 @@ public class Editorial {
         this.direccion = direccion;
     }
 
-    // Getters y setters
     public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-
     public String getDireccion() { return direccion; }
-    public void setDireccion(String direccion) { this.direccion = direccion; }
+
+    @Override
+    public String toString() {
+        return "Editorial[nombre=" + nombre + ", direccion=" + direccion + "]";
+    }
 }
