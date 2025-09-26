23c2cc2a75224df138f1bed731dc52cf643531d1
Giselle <69175205+gisellechaumont@users.noreply.github.com>
2025-09-26 11:09:42 -0300

    Create Reserva.java

---
+++ trabajopractico5/ejercicio6_reserva/Reserva.java
@@ -0,0 +1,35 @@
+public class Reserva {
+    private String fecha;
+    private String hora;
+    private Cliente cliente; // asociación unidireccional
+    private Mesa mesa;       // agregación
+
+    public Reserva(String fecha, String hora, Cliente cliente, Mesa mesa) {
+        this.fecha = fecha;
+        this.hora = hora;
+        this.cliente = cliente;
+        this.mesa = mesa;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public Mesa getMesa() {
+        return mesa;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva [fecha=" + fecha + ", hora=" + hora +
+               ", cliente=" + cliente + ", mesa=" + mesa + "]";
+    }
+}
