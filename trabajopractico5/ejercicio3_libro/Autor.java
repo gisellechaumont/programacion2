557dd8638808ff7d2b2b6955f99c668615196df2
Giselle <69175205+gisellechaumont@users.noreply.github.com>
2025-09-26 10:47:55 -0300

    Create Autor.java

---
+++ trabajopractico5/ejercicio3_libro/Autor.java
@@ -0,0 +1,15 @@
+public class Autor {
+    private String nombre;
+    private String nacionalidad;
+
+    public Autor(String nombre, String nacionalidad) {
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+    }
+
+    // Getters y setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getNacionalidad() { return nacionalidad; }
+    pu
