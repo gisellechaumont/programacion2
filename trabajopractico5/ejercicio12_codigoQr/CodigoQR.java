f1b9b8bf89a712905f355db35464d9105394ebb8
Giselle <69175205+gisellechaumont@users.noreply.github.com>
2025-09-26 11:32:00 -0300

    Create CodigoQR.java

---
+++ trabajopractico5/ejercicio12_codigoQr/CodigoQR.java
@@ -0,0 +1,31 @@
+public class CodigoQR {
+    private String valor;
+    private Usuario usuario;
+
+    public CodigoQR(String valor, Usuario usuario) {
+        this.valor = valor;
+        this.usuario = usuario;
+    }
+
+    // Getters y Setters
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "CodigoQR[valor=" + valor + ", usuario=" + usuario + "]";
+    }
+}
