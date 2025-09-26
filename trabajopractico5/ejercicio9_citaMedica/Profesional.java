9dabaadc429733184608cd92c553944b5078d66c
Giselle <69175205+gisellechaumont@users.noreply.github.com>
2025-09-26 11:18:25 -0300

    Create Profesional.java

---
+++ trabajopractico5/ejercicio9_citaMedica/Profesional.java
@@ -0,0 +1,22 @@
+public class Profesional {
+    private String nombre;
+    private String especialidad;
+
+    public Profesional(String nombre, String especialidad) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Profesional [nombre=" + nombre + ", especialidad=" + especialidad + "]";
+    }
+}
