b633b35d6f817338a7fc81e2b3e0b3f955f0c9a3
Giselle <69175205+gisellechaumont@users.noreply.github.com>
2025-09-26 11:17:01 -0300

    Create FirmaDigital.java

---
+++ trabajopractico5/ejercicio8_documento/FirmaDigital.java
@@ -0,0 +1,29 @@
+public class FirmaDigital {
+    private String codigoHash;
+    private String fecha;
+    private Usuario usuario; // agregación
+
+    public FirmaDigital(String codigoHash, String fecha, Usuario usuario) {
+        this.codigoHash = codigoHash;
+        this.fecha = fecha;
+        this.usuario = usuario;
+    }
+
+    public String getCodigoHash() {
+        return codigoHash;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "FirmaDigital [codigoHash=" + codigoHash + ", fecha=" + fecha + 
+               ", usuario=" + usuario + "]";
+    }
+}
